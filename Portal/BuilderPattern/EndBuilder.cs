08602663db7669db9c8159cee35c3a6b30551129
IdaDania <54858@student.eadania.dk>
2023-05-06 15:44:19 +0200

    added End

---
+++ Portal/BuilderPattern/EndBuilder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Portal.ComponentPattern;
+using PortalGame;
+using PortalGame.BuilderPattern;
+using PortalGame.ComponentPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.BuilderPattern
+{
+    public class EndBuilder : IBuilder
+    {
+        private GameObject gameObject;
+        private Vector2 position;
+
+        public EndBuilder(int x, int y, int tileSize)
+        {
+            position = new Vector2(x * tileSize, y * tileSize);
+        }
+
+        public void BuildGameObject()
+        {
+            gameObject = new GameObject(); 
+
+            BuildComponents();
+        }
+
+        private void BuildComponents()
+        {
+            gameObject.AddComponent(new End(position));
+            gameObject.AddComponent(new SpriteRenderer());
+            gameObject.AddComponent(new Collider()); 
+        }
+
+        public GameObject GetResult()
+        {
+            return gameObject;
+        }
+    }
+}
---
+++ Portal/ComponentPattern/End.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using PortalGame.ComponentPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.ComponentPattern
+{
+    public class End : Component
+    {
+        private Vector2 position;
+        private SpriteRenderer spriteRenderer; 
+
+        public End(Vector2 position)
+        {
+            this.position = position; 
+        }
+
+        public override void Awake()
+        {
+            spriteRenderer = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
+            spriteRenderer.SetSprite("end");
+            spriteRenderer.LayerDepth = 0.5f;
+            spriteRenderer.Scale = 1f;
+
+            GameObject.Transform.Position = position;
+            GameObject.Tag = "End"; 
+        }
+    }
+}
