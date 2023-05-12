10f73a405d51964314f5214dc5da6ca5e7d19fc3
IdaDania <54858@student.eadania.dk>
2023-05-12 22:29:25 +0200

    Wand.cs : Modified * Shoot method added. Uses raycast to determine which platform and side the portal, given as parameter, should be placed.

--- Portal/ComponentPattern/Wand.cs
+++ Portal/ComponentPattern/Wand.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
 using Portal.CreationalPattern;
+using PortalGame;
 using PortalGame.ComponentPattern;
+using PortalGame.ComponentPattern.Portals;
 using PortalGame.CreationalPattern;
-using SharpDX.Direct3D9;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BoundingBox = Microsoft.Xna.Framework.BoundingBox;
+using Ray = Microsoft.Xna.Framework.Ray;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
 
 namespace Portal.ComponentPattern
 {
@@ -40,28 +45,96 @@ namespace Portal.ComponentPattern
             GameObject.Transform.Rotation = (float)Math.Atan2(aimDirection.Y, aimDirection.X);
         }
 
-        public void Shoot(Vector2 direction, BeamType beamType)
+        public void Shoot(PortalType portalType)
         {
-            // use raycast 
+            // define the rays start to the wands position (the Z is set to 0, since we work in 2D)
+            Vector3 originPoint = new Vector3(GameObject.Transform.Position.X, GameObject.Transform.Position.Y, 0);
+            // define the rays direction to the aimdirection (the Z is set to 0, since we work in 2D)
+            Vector3 shootDirection = new Vector3(aimDirection.X, aimDirection.Y, 0); 
+            // set the ray 
+            Ray ray = new Ray(originPoint, shootDirection);
 
-            //Ray ray = new Ray(new Vector3(GameObject.Transform.Position.X, GameObject.Transform.Position.Y, 0), new Vector3(direction.X, direction.Y, 0));
+            // dictionary for hit boundingboxes, and their position 
+            BoundingBox hitBoundingBox = GameWorld.Instance.BoundingBoxes.Keys.First();
 
-            //// calculate which side the portal should spawn 
-            //float? d = ray.Intersects(box);
-            //if (d.HasValue)
-            //{
-            //    Vector3 intersection = ray.Position + ray.Direction * d.Value;
-            //}
+            // get all boundingboxes 
+            foreach (BoundingBox boundingBox in GameWorld.Instance.BoundingBoxes.Keys)
+            {
+                // check for intersection between ray and boundingbox 
+                if(boundingBox.Intersects(ray) != null && 
+                   Vector3.Distance(GameWorld.Instance.BoundingBoxes[boundingBox], ray.Position) < Vector3.Distance(GameWorld.Instance.BoundingBoxes[hitBoundingBox], ray.Position))
+                {
+                    hitBoundingBox = boundingBox; 
+                }
+            }
 
+            // check hit boundingbox again 
+            float? distance = ray.Intersects(hitBoundingBox);
 
-            //if (beamType == BeamType.Red)
-            //{
-            //    RedPortalFactory.Instance.Create();
-            //}
-            //else if (beamType == BeamType.Blue)
-            //{
+            // get the boundingbox that the ray hits 
+            if (distance.HasValue)
+            {
+                // get the point at which the ray intersects the boundingbox 
+                Vector3 intersectionPoint = ray.Position + ray.Direction * distance.Value;
 
-            //}
+                // check which side the portal should be on 
+                Side side = CheckSide(intersectionPoint, GameWorld.Instance.BoundingBoxes[hitBoundingBox]);
+
+                // if no side set, then do not create object 
+                if (side != Side.None)
+                {
+                    GameObject gameObject = new GameObject();
+
+                    // create gameobject via portal factory, depending on portal type 
+                    if (portalType == PortalType.Red)
+                    {
+                        gameObject = RedPortalFactory.Instance.Create(side);
+                    }
+                    else if (portalType == PortalType.Blue)
+                    {
+                        gameObject = BluePortalFactory.Instance.Create(side);
+                    }
+
+                    // set the portals position at intersectionpoint 
+                    gameObject.Transform.Position = new Vector2(intersectionPoint.X, intersectionPoint.Y);
+
+                    // add portal object to GameWorld 
+                    GameWorld.Instance.Instantiate(gameObject);
+                }
+            }
+        }
+
+        private Side CheckSide(Vector3 intersectionPoint, Vector3 platformCenter)
+        {
+            float xDiff = intersectionPoint.X - platformCenter.X; 
+            float yDiff = intersectionPoint.Y - platformCenter.Y; 
+            float zDiff = intersectionPoint.Z - platformCenter.Z; 
+
+            // check which side the intersectionpoint is closest to 
+            if(Math.Abs(xDiff) > Math.Abs(yDiff))
+            {
+                if(xDiff > 0)
+                {
+                    return Side.Right; 
+                }
+                else
+                {
+                    return Side.Left; 
+                }
+            }
+            else if(Math.Abs(yDiff) > Math.Abs(zDiff))
+            {
+                if(yDiff > 0)
+                {
+                    return Side.Bottom;
+                }
+                else
+                {
+                    return Side.Top;
+                }
+            }
+
+            return Side.None; 
         }
     }
 }
