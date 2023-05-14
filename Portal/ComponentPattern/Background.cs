86c99ac004d98695c7d2ff8e2c720869b2d0c9e5
IdaDania <54858@student.eadania.dk>
2023-05-14 14:53:09 +0200

    added background

---
+++ Portal/ComponentPattern/Background.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using PortalGame;
+using PortalGame.ComponentPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.ComponentPattern
+{
+    public class Background : Component
+    {
+        private SpriteRenderer spriteRenderer;
+
+        public override void Awake()
+        {
+            spriteRenderer = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
+            spriteRenderer.SetSprite("BG\\bg");
+            spriteRenderer.Scale = 1f;
+            spriteRenderer.LayerDepth = 0.1f;
+            GameObject.Transform.Position = new Vector2(GameWorld.Instance.LevelSize.X / 2, GameWorld.Instance.LevelSize.Y / 2);
+        }
+    }
+}
